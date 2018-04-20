3489eadaef3ca808214c7c7a109efeb13e0e3975
duolaAOA <1219960386@qq.com>
2018-04-20 21:59:19 +0800

    add settings file

---
+++ comm/settings.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8 -*-
+
+# 驱动路径
+DRIVER_DIR = "E:/envirtual"
+# 数据库名
+DB_NAME = "market_price"
+# 数据表名
+TABLE_NAME = "test"
+# 微博登录入口
+LOGIN_URL = "https://passport.weibo.cn/signin/login"
+# 登录用户名
+USERNAME = ""
+# 登录密码
+PASSWORD = ""
+# 关键词搜索入口
+TOPIC_BASE_URL = "https://weibo.cn/search/mblog?hideSearchFrame=&keyword={}&page={}"
+# 关键词
+TOPIC_KEYWORD = "鸿茅药酒"
+# 最大爬取深度
+MAX_PAGE = 236
